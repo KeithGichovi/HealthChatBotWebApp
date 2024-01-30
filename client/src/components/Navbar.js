f9fe60c032c25b15e38d906967be9f3e742c12f4
KeithGichovi <kgichovi04@gmail.com>
2024-01-30 14:18:27 +0000

    initial commit

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+            <ul>
+                <li>home</li>
+                <li>Register </li>
+                <li>Login</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar
